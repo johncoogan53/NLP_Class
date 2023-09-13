492d9c963f4a08ca090e13c36678f4a2864c0fd2
johncoogan53 <john.h.coogan@protonmail.com>
2023-09-13 16:42:16 -0400

    "adsfadsf"

--- MarkovGen/mtg.py
+++ MarkovGen/mtg.py
@@ -5,6 +5,7 @@ args:
     n [int]: length of n-grams to use for prediction
     corpus: source corpus [list of tokens]
     randomize: stochastic or deterministic subsequent word selection"""
+import numpy as np
 
 
 def finish_sentence(sentence, n, corpus, randomize=False):
@@ -13,4 +14,31 @@ def finish_sentence(sentence, n, corpus, randomize=False):
     """
     final_sentence = ""
 
+    # take a sentence of tokens and predict the next word in that sentence [list] of tokens based
+    # on 'n' past words
+    # take the corpus and make a vocabulary (only unique items)
+    corpus_array = np.array(corpus)
+    vocabulary = np.unique(corpus_array)
+    print(corpus_array)
+    sentence_array = np.array(sentence)
+
+    # assign probabilities to each vocabulary word based off occurence within the corpus
+    prob = np.empty_like(vocabulary)
+    for i in vocabulary:
+        p[i] = np.count_nonzero(corpus_array[i] == vocabulary[i])
+    print(p)
+    exit()
     return final_sentence
+
+
+def main():
+    corp = ["hello", "the", "the", "hello", "goodbye", "a", "?", "ok"]
+    test_n = 2
+    sent = ["goodbye", "hello"]
+    finish_sentence(sent, test_n, corp)
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
